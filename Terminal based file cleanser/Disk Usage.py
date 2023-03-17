339ea5ae99dbbb53258a7790e15c69c28e117985
codit04 <lalitmohan040403@gmail.com>
2023-03-17 20:17:53 +0530

    Additional Utilities

---
+++ Terminal based file cleanser/Disk Usage.py
@@ -0,0 +1,32 @@
+import psutil
+from blessings import Terminal
+
+
+term=Terminal()
+
+width=term.width
+height=term.height
+
+
+
+def barGraph(percent):
+    graphWidth=int(percent*(width-15))
+    return '['+'#' * graphWidth+' ' * (width-10-graphWidth) + ']'
+
+while True:
+    with term.fullscreen():
+        mem = psutil.disk_usage('/')
+        used_percent = mem.percent
+
+        print(term.clear())
+        print(term.move(0, 0))
+
+        print('Disk Usage: {}%'.format(used_percent))
+        print(barGraph(used_percent / 100))
+
+        psutil.cpu_percent(interval=1)
+
+        if term.inkey() is not None:
+            break
+
+
---
+++ Terminal based file cleanser/Memory Usage.py
@@ -0,0 +1,34 @@
+
+import psutil
+from blessings import Terminal
+import sys
+import tty
+import termios
+
+term = Terminal()
+
+width = term.width
+height = term.height
+
+def draw_bar_graph(percent):
+    bar_width = int(percent * (width - 10))
+    return '[' + '#' * bar_width + ' ' * (width - 10 - bar_width) + ']'
+
+while True:
+
+    with term.fullscreen():
+        # Get current memory usage
+        mem = psutil.virtual_memory()
+        used_percent = mem.percent
+
+        print(term.clear())
+        print(term.move(0, 0))
+
+        print('Memory Usage: {}%'.format(used_percent))
+        print(draw_bar_graph(used_percent / 100))
+
+        psutil.cpu_percent(interval=1)
+
+        if term.inkey() is not None:
+            break
+
